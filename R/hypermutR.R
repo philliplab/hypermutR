859061cc5eacb91c44417f269ff9c62d3894ec9e
Phillip Labuschagne <philliplab@gmail.com>
2017-11-21 04:56:49 +0200

    Add consensus generation

--- R/hypermutR.R
+++ R/hypermutR.R
@@ -4,6 +4,18 @@
 #' @export
 
 remove_hypermut <- function(dat){
+  cons_ints <- apply(consensusMatrix(dat), 2, function(x){which(x == max(x))[1]})
+  cons <- paste(row.names(consensusMatrix(dat))[cons_ints], collapse = '')
+  rm(cons_ints)
+  
   ddat <- deduplicate_seqs(dat)
+  for (i in length(ddat)){
+  }
   return(NULL)
+  if (FALSE){
+    # TEMP store for testing/debugging code
+    # to be moved to unit tests sooner
+    remove_hypermut(ld_seqs)
+    dat <- ld_seqs
+  }
 }
