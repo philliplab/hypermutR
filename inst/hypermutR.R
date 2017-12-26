f56a3787eb483585d4088eba3520bcb351c85fff
Phillip Labuschagne <philliplab@gmail.com>
2017-12-26 19:35:03 +0200

    update UI

--- inst/hypermutR.R
+++ inst/hypermutR.R
@@ -3,23 +3,37 @@
 suppressPackageStartupMessages(library("optparse"))
 
 option_list <- list(
-  make_option("--file_name", 
+  make_option("--input_file", 
               help = "Path to input file and file name"),
-  make_option("--output_name", 
-              help = "Path and name of output file")
+  make_option("--output_file", 
+              help = "Path and name of output file"),
+  make_option("--p_value",
+              help = "The p-value to apply to the one-sided fischer test"),
+  make_option("--ancestor",
+              default = "consensus",
+              help = "Either 'consensus' to indicate that the consensus sequences must be computed, or 'first' to indicate that the first sequence in the dataset should be considered to be the ancestral sequence, or the ancestral sequence itself."),
+  make_option("--fix_with",
+              default = NULL,
+              help = "If omitted, hypermutants will be removed. If a single letter is specified, then hypermutants will be 'corrected' by replacing the hypermutated base with the specified letter.")
 )
 
+opt <- list(file_name = "/home/phillipl/projects/hypermutR/data/ld_seqs.fasta", ancestor = "consensus", output_name = '/tmp/bla.fasta')
 opt <- parse_args(OptionParser(option_list = option_list,
   description = "Cast all hypermutants out into the cold!",
   epilogue = "Example Call:
-hypermutR.R --file_name=/path/to/file.fasta --output_name=/path/to/out_file.fasta"))
+hypermutR.R --input_file=/path/to/file.fasta --output_file=/path/to/out_file.fasta --p_value=0.1 --ancestor=first --fix_with=r"))
 
 suppressPackageStartupMessages(library("hypermutR"))
 
+if(is.null(opt$fix_with)){opt$fix_with <- FALSE}
+
 dat <- read.fasta(opt$file_name)
-cdat <- remove_hypermut(dat)
-writeXStringSet(cdat$seq_results, opt$output_name, width=80)
-writeXStringSet(cdat$seq_hypermutants, 
-                gsub(".fasta", "_hypermutants.fasta", opt$output_name), 
-                width=80)
+cdat <- remove_hypermut(dat, ancestor = opt$ancestor, fix_with=opt$fix_with)
+names(cdat)
+write.fasta(cdat$seq_result, names(cdat$seq_result), opt$output_name)
+if (!is.null(cdat$seq_hypermutants)){
+  write.fasta(cdat$seq_hypermutants, names(cdat$seq_hypermutants), 
+              gsub(".fasta", "_hypermutants.fasta", opt$output_name))
+}
+write.csv(cdat$all_mut_pos, gsub('.fasta', '_mut_pos.csv', opt$output_name), row.names=F)
 
