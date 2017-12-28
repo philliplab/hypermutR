e4a7282b1f3b6ec58371f21aaf472d3c4fbae6fb
Phillip Labuschagne <philliplab@gmail.com>
2017-12-28 11:23:10 +0200

    bug fix in UI; adds some input checking to UI

--- inst/hypermutR.R
+++ inst/hypermutR.R
@@ -18,23 +18,33 @@ option_list <- list(
               help = "If omitted, hypermutants will be removed. If a single letter is specified, then hypermutants will be 'corrected' by replacing the hypermutated base with the specified letter.")
 )
 
-opt <- list(file_name = "/home/phillipl/projects/hypermutR/data/ld_seqs.fasta", ancestor = "consensus", output_name = '/tmp/bla.fasta')
+if (FALSE){
+  # some debugging code that does not get run
+  opt <- list(input_file = "/home/phillipl/projects/hypermutR/data/ld_seqs.fasta", ancestor = "consensus", output_file = '/tmp/bla.fasta')
+}
+
 opt <- parse_args(OptionParser(option_list = option_list,
   description = "Cast all hypermutants out into the cold!",
   epilogue = "Example Call:
-hypermutR.R --input_file=/path/to/file.fasta --output_file=/path/to/out_file.fasta --p_value=0.1 --ancestor=first --fix_with=r"))
+hypermutR.R --input_file=/path/to/file.fasta --output_file=/path/to/out_file.fasta --p_value=0.1 --ancestor=first --fix_with=r
+
+hypermutR.R --input_file=/home/phillipl/projects/hypermutR/data/ld_seqs.fasta --output_file=/tmp/ld_seqs_test.fasta --ancestor=consensus
+
+File extensions must be specified in lower case."))
 
 suppressPackageStartupMessages(library("hypermutR"))
 
 if(is.null(opt$fix_with)){opt$fix_with <- FALSE}
+if(!grepl('.fasta$', opt$input_file)){stop('input_file must end in .fasta (lowercase)')}
+if(!grepl('.fasta$', opt$output_file)){stop('ouput_file must end in .fasta (lowercase)')}
 
-dat <- read.fasta(opt$file_name)
+dat <- read.fasta(opt$input_file)
 cdat <- remove_hypermut(dat, ancestor = opt$ancestor, fix_with=opt$fix_with, p_value = opt$p_value)
 names(cdat)
-write.fasta(cdat$seq_result, names(cdat$seq_result), opt$output_name)
+write.fasta(cdat$seq_result, names(cdat$seq_result), opt$output_file)
 if (!is.null(cdat$seq_hypermutants)){
   write.fasta(cdat$seq_hypermutants, names(cdat$seq_hypermutants), 
-              gsub(".fasta", "_hypermutants.fasta", opt$output_name))
+              gsub(".fasta", "_hypermutants.fasta", opt$output_file))
 }
-write.csv(cdat$all_mut_pos, gsub('.fasta', '_mut_pos.csv', opt$output_name), row.names=F)
+write.csv(cdat$all_mut_pos, gsub('.fasta', '_mut_pos.csv', opt$output_file), row.names=F)
 
